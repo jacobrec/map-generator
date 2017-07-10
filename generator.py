8e878eeb4439b92953be75e4d5ebba758c1b2e04
Peter Elliott <p.elliott332@gmail.com>
2017-07-10 16:58:33 -0600

    added preliminary multithreading support

--- generator.py
+++ generator.py
@@ -1,33 +1,38 @@
 from opensimplex import OpenSimplex
+from multiprocessing import Pool
+from functools import partial
 
 
 class Generator():
     complexity = 5
+
     def __init__(this, seed):
         this.gen = OpenSimplex(seed)
-    def generateHeightmap(this,size, featuresize, detail):
+
+    def generateHeightmap(this, size, featuresize, detail):
         width = size
         height = size
-        value = []
-        for x in range(width):
-            value.append([]);
-            for y in range(height):
-                value[x].append(0)
 
+        p_genColumn = partial(this.genColumn, width, height, detail, featuresize)
 
-        for y in range(height):
-            for x in range(width):
-                nx = x/width - 0.5
-                ny = y/height - 0.5
-                a = 0
-                b = 0
-                for t in range(-featuresize,detail-featuresize):
-                    n = 2**t
-                    b += 1/n
-                    a += this.noise(n * nx, n * ny)/n
-                value[x][y] = a/b
+        with Pool() as p:
+            value = list(p.map(p_genColumn, range(width)))
 
         return value
 
-    def noise(this,nx, ny, scale = 255):
+    def noise(this, nx, ny, scale=255):
         return int((this.gen.noise2d(nx*this.complexity, ny*this.complexity) / 2.0 + 0.5)*scale)
+
+    def genColumn(this, width, height, detail, featuresize, x):
+        col = [0]*height
+        for y in range(height):
+            nx = x/width - 0.5
+            ny = y/height - 0.5
+            a = 0
+            b = 0
+            for t in range(-featuresize, detail-featuresize):
+                n = 2**t
+                b += 1/n
+                a += this.noise(n * nx, n * ny)/n
+            col[y] = a/b
+        return col
